3310042cc531437f6b502c11af56fd9488b44de3
Vimal Tagline <vimal.tagline@gmail.com>
2020-11-24 15:52:44 +0530

    image path set

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
+const path = require('path')
+const express = require('express')
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(router);
+// server.use(router);
+server.use('', express.static(path.join(__dirname, 'images/')))
 
+// Avoid CORS issue
+server.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// server.use(jsonServer.rewriter(routes))
+server.use(router) 
 server.listen(port);
