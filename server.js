109950b62a6e0c02f0bfc2f8c3080920932fae9b
Vimal Tagline <vimal.tagline@gmail.com>
2020-11-26 10:33:37 +0530

    all records remove from business units

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ server.use('', express.static(path.join(__dirname, 'images/')))
 
 // Avoid CORS issue
 server.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*");
     next();
 });
 
